e1113b8bcb9279e18cd9ca24b7a976d8a93f2091
Debiprasad2002 <debiprasadbedanta@gmail.com>
2023-12-18 22:23:43 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+import winter from "./images/winter.jpg"
+import summer from "./images/summer.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =() =>{
+
+
+  const [latitude , setLatitude] = useState(0)
+  const [longitude , setLongitude] = useState(0)
+  const [hemisphere, setHemisphere] = useState("")
+  const [month , setMonth] = useState(()=>{return (new Date().getMonth()+1)})
+
+
+   function fetchlocation(){
+    if(navigator.geolocation){
+           navigator.geolocation.getCurrentPosition(
+             (position=>{
+                setLatitude(position.coords.latitude)
+                setLongitude(position.coords.longitude)
+                
+
+
+                if(latitude>0){
+                    setHemisphere("Northen Hemisphere")
+                }
+                else if(latitude<0){
+                    setHemisphere("Southern Hemisphere")
+                }
+                else{
+                    setHemisphere("Equator")
+                }
+             }
+
+             )
+           )
+    }
+    else{
+        alert("Geolocations is not supported at your browser")
+    }
+   }
+
+    return(
+        <div>
+        <button onClick={fetchlocation}>Fetch Location</button>
+        <h1>Latitude :{latitude}</h1>
+        <h1>Longitude:{longitude}</h1>
+        <h1>Hemisphere:{hemisphere}</h1>
+        <h1>Month: {month}</h1>
+        {
+            hemisphere && (hemisphere ==="Northen Hemisphere" && month>=4 && month<=10) &&
+            (
+                <div>
+                    <h1>Summer Season </h1>
+                    <img src="{summer}" alt="summer"/>
+                </div>
+            )
+        }
+        {
+            hemisphere && (hemisphere ==="Norther Hemisphere" && (month<4 || month>10))&&
+            (
+                <div>
+                   <h1>winter Season</h1>
+                   <img src="{winter}" alt="winter"/>
+                </div>
+            )
+        }
+
+        </div>
+    )
 }
-
 export default App;
